a99a219627ac62667891b5dc19d2e3a07ed5833f
MMarioB <mmariob196@gmail.com>
2024-07-21 17:21:41 +0200

    setup deploy

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://tu-api-palabras.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  base: '/complicidad-ganadora/',
 })
